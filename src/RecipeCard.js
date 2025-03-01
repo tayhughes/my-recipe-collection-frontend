ddc61b603a726eb5737e2ff056de08721ef23bbd
Taylor Hughes <taylorhughess.tjh@gmail.com>
2025-03-01 11:55:13 -0800

    Show the recipe card with ordered instructions and looks like a sheet of paper

--- src/RecipeCard.js
+++ src/RecipeCard.js
@@ -1,10 +1,7 @@
 import React, {useState, useEffect} from 'react';
-import './RecipeCard.css';
+import {SERVER_IP_ADDRESS, SERVER_PORT} from './ServerConfigs.js';
+import './styles/RecipeCard.css';
 
-const SERVER_IP_ADDRESS = 'http://192.168.1.168';
-//const SERVER_IP_ADDRESS = 'http://192.168.0.13';
-//const SERVER_IP_ADDRESS = 'http://10.1.10.212';
-const SERVER_PORT = '3001';
 const POST_REQ_PATH = '/recipe_id=';
 const SERVER_ADDRESS = `${SERVER_IP_ADDRESS}:${SERVER_PORT}${POST_REQ_PATH}`;
 
@@ -75,6 +72,12 @@ function RecipeCard({selectRecipe}){
 
     if(awaitData){
         return(<p>Click "view recipe" to see a select recipe</p>);
+    }
+
+    if(data.length === 0){
+        return(
+            <p>Recipe No Longer Exists -- Try refreshing screen</p>
+        );
     }
 
     return(
