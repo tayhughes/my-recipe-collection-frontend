7de8f3ee6d574b36a2fc3bf5dc1a7f12e1dc59b7
Taylor Hughes <taylorhughess.tjh@gmail.com>
2025-03-01 11:51:52 -0800

    centralized the server configurations to update what subnet the app is on

---
+++ src/ServerConfigs.js
@@ -0,0 +1,5 @@
+export const SERVER_IP_ADDRESS = 'http://192.168.1.168';
+//export const SERVER_IP_ADDRESS = 'http://192.168.0.13';
+//export const SERVER_IP_ADDRESS = 'http://10.1.10.212';
+//export const SERVER_IP_ADDRESS = 'http://192.168.1.168';
+export const SERVER_PORT = '3001';
